1609e6728f28b9476f5f325f9927872184a916b4
rspaeth79 <rspaeth@mvqn.net>
2019-09-09 02:21:18 -0700

    - Completed Robo Task: SFTP Configuration

--- RoboFile.php
+++ RoboFile.php
@@ -10,39 +10,57 @@ use Robo\Tasks;
  */
 class RoboFile extends Tasks
 {
-    #region Hooks
+    #region Hook Simulation
 
-    function hookInstall()
+    /**
+     * Simulates the Plugin being installed by executing the "hook_install.php" script.
+     */
+    public function hookInstall()
     {
         $this->say("Executing the Plugin's install hook...");
         $this->_exec("php ./src/hook_install.php");
     }
 
-    function hookRemove()
+    /**
+     * Simulates the Plugin being removed by executing the "hook_remove.php" script.
+     */
+    public function hookRemove()
     {
         $this->say("Executing the Plugin's remove hook...");
         $this->_exec("php ./src/hook_remove.php");
     }
 
-    function hookUpdate()
+    /**
+     * Simulates the Plugin being updated by executing the "hook_update.php" script.
+     */
+    public function hookUpdate()
     {
         $this->say("Executing the Plugin's update hook...");
         $this->_exec("php ./src/hook_update.php");
     }
 
-    function hookEnable()
+    /**
+     * Simulates the Plugin being enabled by executing the "hook_enable.php" script.
+     */
+    public function hookEnable()
     {
         $this->say("Executing the Plugin's enable hook...");
         $this->_exec("php ./src/hook_enable.php");
     }
 
-    function hookDisable()
+    /**
+     * Simulates the Plugin being disabled by executing the "hook_disable.php" script.
+     */
+    public function hookDisable()
     {
         $this->say("Executing the Plugin's disable hook...");
         $this->_exec("php ./src/hook_disable.php");
     }
 
-    function hookConfigure()
+    /**
+     * Simulates the Plugin being configured by executing the "hook_configure.php" script.
+     */
+    public function hookConfigure()
     {
         $this->say("Executing the Plugin's configure hook...");
         $this->_exec("php ./src/hook_configure.php");
@@ -50,43 +68,62 @@ class RoboFile extends Tasks
 
     #endregion
 
+    #region Environment
 
-
-
-
-    function sftpConfigure()
+    /**
+     * @param string $key
+     * @param string $label
+     * @param string $file
+     * @param bool $quotes
+     */
+    private function envEntry(string $key, string $label, string &$file, bool $quotes = false)
     {
-        $host = $this->ask("SFTP Host");
-        $port = $this->askDefault("SFTP Port", "22");
-        $user = $this->ask("SFTP User");
-        $pass = $this->ask("SFTP Pass", true);
+        $found = preg_match('/^('.$key.')[ \t]*=[ \t]*(.*)$/m', $file, $matches);
+        $match = $found ? $matches[2] : "";
+        $plain = $found ? str_replace("\"", "", $match) : "";
 
+        $value = str_replace("\"", "", $this->askDefault($label, $plain ?? ""));
+
+        if($found && $plain !== $value)
+        {
+            $file = str_replace(
+                $matches[0],
+                str_replace(
+                    $matches[2] === "\"\"" ? "\"\"" : str_replace("\"", "", $matches[2]),
+                    $matches[2] === "\"\"" ? "\"$value\"" : $value,
+                    $matches[0]
+                ),
+                $file
+            );
+        }
+
+        if(!$found)
+            $file .= "\n$key=".($quotes ? "\"" : "")."$value".($quotes ? "\"" : "")."\n";
+    }
+
+    #endregion
+
+    #region SFTP
+
+    /**
+     * Prompts the developer for SFTP Configuration and saves or updates the results in the "src/.env" file.
+     */
+    public function sftpConfigure()
+    {
         $envPath = __DIR__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR.".env.local";
         $envFile = file_exists($envPath) ? file_get_contents($envPath) : "";
 
-        if(preg_match('/^(SFTP_HOST)[ \t]*=[ \t]*(.*)$/m', $envFile, $matches))
-            $envFile = str_replace($matches[0], str_replace($matches[2], $host, $matches[0]), $envFile);
-        else
-            $envFile .= "\nSFTP_HOST=$host\n";
-
-        if(preg_match('/^(SFTP_PORT)[ \t]*=[ \t]*(.*)$/m', $envFile, $matches))
-            $envFile = str_replace($matches[0], str_replace($matches[2], $host, $matches[0]), $envFile);
-        else
-            $envFile .= "SFTP_PORT   = $port\n";
-
-        if(preg_match('/^(SFTP_USER)[ \t]*=[ \t]*(.*)$/m', $envFile, $matches))
-            $envFile = str_replace($matches[0], str_replace($matches[2], $host, $matches[0]), $envFile);
-        else
-            $envFile .= "SFTP_USER   = $user\n";
-
-        if(preg_match('/^(SFTP_PASS)[ \t]*=[ \t]*(.*)$/m', $envFile, $matches))
-            $envFile = str_replace($matches[0], str_replace($matches[2], $host, $matches[0]), $envFile);
-        else
-            $envFile .= "SFTP_PASS   = $pass\n";
+        $this->envEntry("SFTP_HOST", "SFTP Host", $envFile);
+        $this->envEntry("SFTP_PORT", "SFTP Port", $envFile);
+        $this->envEntry("SFTP_USER", "SFTP User", $envFile);
+        $this->envEntry("SFTP_PASS", "SFTP Pass", $envFile, true);
 
         file_put_contents($envPath, $envFile, LOCK_EX);
     }
 
+
+
+    #endregion
 
 
 }
